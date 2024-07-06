6c19c8b4f70a1625eae3f5bc25c108942edd11fe
alimertcankaraman <alimertcankaraman@gmail.com>
2024-07-06 14:53:21 +0300

    9th week works are added.

---
+++ 09_25.04.2022/objectInit.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+class MyClass
+{
+public:
+    MyClass(int a = 1)
+    {
+        std::cout << " param: " << a << "\n";
+    }
+};
+
+
+
+int main()
+{
+    MyClass x1 = 10.1;  //double verilirse warning only!
+    MyClass x2(0);
+    MyClass x3{30};     //double verilirse error!
+    MyClass xn[10]{ 25 };
+    return 0;
+}
