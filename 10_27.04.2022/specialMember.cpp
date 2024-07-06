124b8da8b8c4870ed688ed639b633bc40c221f8e
alimertcankaraman <alimertcankaraman@gmail.com>
2024-07-06 14:53:42 +0300

    10th week works are added.

---
+++ 10_27.04.2022/specialMember.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+class MyClass
+{
+public:
+    MyClass(int x) : MyClass(x, 0) {}
+    MyClass() = default;
+    //MyClass() = delete; //can not be called
+    MyClass(int x, int y) : m{x}, n{y} {}   //!delegating ctor
+    void printM()
+    {
+        std::cout << "m is " << m << "\n";
+        std::cout << "n is " << n << "\n";
+    }
+
+private:
+    int m, n;
+};
+
+
+
+int main()
+{
+    MyClass x1;
+    MyClass x2(2);
+    x1.printM();
+    x2.printM();
+    std::cout << "babababa";
+}
